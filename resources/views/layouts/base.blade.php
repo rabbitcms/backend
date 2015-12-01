97bb3e91da9a4d3cf55cf3e1fbcc6f920295dbeb
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2015-12-01 19:44:37 +0200

    Fix unsecure resources

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="" />
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
     @foreach(\RabbitCMS\Backend\Support\Metronic::css() as $css)
         <link href="{{\RabbitCMS\Backend\Support\Metronic::base()}}{{$css}}" rel="stylesheet" type="text/css"/>
     @endforeach
