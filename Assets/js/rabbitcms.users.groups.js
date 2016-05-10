bc98e20e5676b03ff611baba68d62a0eb1714838
caxapok <dmytro.zhurakovsky@gmail.com>
2016-05-10 12:01:20 +0300

    permissions checkbox fix

--- Assets/js/rabbitcms.users.groups.js
+++ Assets/js/rabbitcms.users.groups.js
@@ -61,6 +61,45 @@ define(["jquery"], function ($) {
                 });
             }
         });
+
+        portlet.on('change', '.write-rule', function () {
+            if ($(this).is(':checked')) {
+                $(this).parents('tr').find('.read-rule').prop('checked', true);
+                Metronic.updateUniform();
+            }
+        });
+
+        portlet.on('change', '.read-rule', function () {
+            var _write = $(this).parents('tr').find('.write-rule');
+            if (_write.is(':checked')) {
+                $(this).prop('checked', true);
+                Metronic.updateUniform();
+            }
+        });
+
+        portlet.on('change', '.module-read-rule', function () {
+            var _module = $(this).data('module');
+            var _write = $(this).parents('tr').find('.module-write-rule');
+
+            if (_write.is(':checked')) {
+                $(this).prop('checked', true);
+            }
+
+            $('.' + _module + '.read-rule').prop('checked', $(this).prop('checked')).trigger('change');
+
+            Metronic.updateUniform();
+        });
+
+        portlet.on('change', '.module-write-rule', function () {
+            var _module = $(this).data('module');
+            if ($(this).is(':checked')) {
+                $(this).parents('tr').find('.module-read-rule').prop('checked', true);
+            }
+
+            $('.' + _module + '.write-rule').prop('checked', $(this).prop('checked')).trigger('change');
+
+            Metronic.updateUniform();
+        });
     }
 
     var MicroEvent = new RabbitCMS.MicroEvent({table: table, form: form});
