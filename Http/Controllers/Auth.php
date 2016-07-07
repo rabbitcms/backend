ceb2b9d49df47bd28af8345f12c8cdf631b96ad9
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2016-07-07 22:47:36 +0300

    Extend base backend controller.

--- Http/Controllers/Auth.php
+++ Http/Controllers/Auth.php
@@ -23,7 +23,7 @@ class Auth extends Controller
 
     public function getLogin()
     {
-        return \View::make('backend::auth.login');
+        return $this->view('auth.login');
     }
 
     public function postLogin(Request $request)
--- Http/Controllers/Controller.php
+++ Http/Controllers/Controller.php
@@ -2,9 +2,35 @@
 
 namespace RabbitCMS\Backend\Http\Controllers;
 
+use Illuminate\Auth\AuthManager;
+use Illuminate\Foundation\Application;
+use RabbitCMS\Backend\Entities\User;
 use RabbitCMS\Carrot\Http\ModuleController;
 
 abstract class Controller extends ModuleController
 {
     protected $module = 'backend';
+
+    /**
+     * @var \Illuminate\Contracts\Auth\Guard|\Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected $auth;
+
+    protected $denyView = 'backend::deny';
+
+    public function __construct(Application $app, AuthManager $auth)
+    {
+        $this->auth = $auth->guard('backend');
+        parent::__construct($app);
+    }
+
+    /**
+     * Get current user.
+     *
+     * @return User|null
+     */
+    protected function user()
+    {
+        return $this->auth->user();
+    }
 }
