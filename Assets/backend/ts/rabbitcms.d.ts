d2e498604ce733e03a11a7ad21d180352fd8acbc
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2016-07-28 22:32:29 +0300

    Add module definitions.

--- Assets/backend/ts/rabbitcms.d.ts
+++ Assets/backend/ts/rabbitcms.d.ts
@@ -8,145 +8,125 @@ declare module DataTables {
 }
 
 declare module "rabbitcms.backend" {
-    export interface Handler {
-        handler:string;
-        regex?:RegExp;
-        module:string;
-        exec?:string;
-        ajax?:boolean;
-        pushState?:boolean;
-        permanent?:boolean;
-        widget?:JQuery;
-    }
-
-    export interface BlockUIOptions {
-        message?:string;
-        boxed?:boolean;
-        animate?:boolean;
-        iconOnly?:boolean;
-        textOnly?:boolean;
-        target?:JQuery | string;
-        zIndex?:number;
-        cenrerY?:boolean;
-        overlayColor?:string;
-    }
-
-    export interface ValidationOptions extends JQueryValidation.ValidationOptions {
-        completeSubmit?:() => void;
-        state?:State;
-    }
-
     export interface RabbitCMSOptions {
-        handlers?:Handler[];
-        path?:string;
+        handlers?: Handler[];
+        path?: string;
     }
-
     export class State {
-        link:string;
-        handler:Handler;
-        checkers:Promise<void>[];
-
-        constructor(link:string, handler:Handler);
-
-        public check():Promise<void[]>;
-
-        public addChecker(promise:Promise<void>);
+        link: string;
+        handler: Handler;
+        checkers: ((replay: ReplayFunc) => Promise<void>)[];
+        widget: JQuery;
+        constructor(link: string, handler: Handler, widget: JQuery);
+        check(replay: ReplayFunc): Promise<void[]>;
+        addChecker(checker: (replay: ReplayFunc) => Promise<void>): void;
     }
-
-    export class RabbitCMS {
-        static init(options?:RabbitCMSOptions):void;
-
-        static ready():void;
-
-        static getPath():string;
-
-        static setToken(value:string):void;
-
-        static getToken():string;
-
-        static navigate(link:string, pushState?:boolean):boolean;
-
-        static showPortlet(h:Handler, widget:JQuery):boolean;
-
-        static loadModalWindow(link:any, callback:any):void;
-
-        static showModal(modal:JQuery):void;
-
-        static alert(options:any):string;
-
-        static message(options:any):void;
-
-        static dangerMessage(message:any, container?:any):void;
-
-        static scrollTo(element?:JQuery, offset?:number):void;
-
-        static scrollTop():void;
-
-        static getViewPort():{
-            width:any;
-            eight:any;
-        };
-
-        static blockUI(target?:JQuery | string, options?:BlockUIOptions):void;
-
-        static unblockUI(target?:any):void;
-
-        static canSubmit:{
-            _match:boolean;
-            init():void;
-            check(event:any):boolean;
-        };
-
-        static submitForm(form:any, callback:any):void;
-
-        static ajax(link:string, callback:any):void;
-
-        static ajaxPost(link:string, data:Object, callback:any):void;
-
-        static validate(form:JQuery, options?:ValidationOptions):void;
-
-        static Dialogs:{
-            onDelete:(link:any, callback:any) => void;
-            onConfirm:(message:any, callback:any) => void;
-        };
-        static Tools:{
-            transliterate:(string:any, spase:any) => string;
-        };
-
-        static select2(selector:JQuery, options?:Select2Options):void;
+    export enum StateType {
+        NoPush = 0,
+        None = 1,
+        Push = 2,
+        Replace = 3,
     }
-
-    export class MicroEvent {
-        constructor(object?:Object);
-
-        bind(event:string, fct:any):void;
-
-        unbind(event:string, fct:any):void;
-
-        trigger(event:string, ...args:any[]):void;
+    export interface ReplayFunc {
+        (): void;
     }
-
     export interface AjaxSettings extends JQueryAjaxSettings {
-        warningTarget?:JQuery;
+        warningTarget?: JQuery;
     }
-
+    export class RabbitCMS {
+        private static _path;
+        private static _stack;
+        private static menu;
+        static init(options?: RabbitCMSOptions): void;
+        static ready(): void;
+        static findHandler(link: string): Handler;
+        static setPath(path: string): void;
+        static getPath(): string;
+        static setToken(value: string): void;
+        static getToken(): string;
+        static loadModuleByHandler(handler: Handler, widget: JQuery, state: State): void;
+        static loadModule(widget: JQuery): void;
+        static navigateByHandler(h: Handler, link: string, pushState: StateType, replay: ReplayFunc): Promise<boolean>;
+        static navigate(link: string, pushState?: StateType, replay?: ReplayFunc): Promise<boolean>;
+        static showPortlet(h: Handler, widget: JQuery): boolean;
+        static loadModalWindow(link: string, callback: (model: JQuery, data: any, textStatus: string, jqXHR: JQueryXHR) => any): void;
+        static showModal(modal: JQuery): void;
+        static getUniqueID(prefix?: string): string;
+        static alert(options: any): string;
+        static message(options: any): void;
+        static dangerMessage(message: string, target?: JQuery): void;
+        static scrollTo(element?: JQuery, offset?: number): void;
+        static scrollTop(): void;
+        static getViewPort(): {
+            width: any;
+            eight: any;
+        };
+        static updatePlugins(target?: JQuery): void;
+        static initSidebar(): void;
+        static setMenu(menu: string): void;
+        static blockUI(target?: JQuery | string, options?: BlockUIOptions): void;
+        static unblockUI(target?: any): void;
+        static submitForm(form: any, callback: any): void;
+        static ajaxPost(link: string, data: Object, callback: any): void;
+        static ajax(options: AjaxSettings): void;
+        static validate(form: JQuery, options?: ValidationOptions): void;
+        static Dialogs: {
+            onDelete: (link: any, callback: any) => void;
+            onConfirm: (message: any, callback: any) => void;
+        };
+        static select2(selector: JQuery, options?: Select2Options): void;
+    }
+    export class Tools {
+        static transliterate(string: string, spase?: string): string;
+    }
     export interface FormOptions {
-        validation?:ValidationOptions;
-        ajax?:AjaxSettings|boolean;
-        state?:State;
-        completeSubmit?:()=>void;
+        validation?: ValidationOptions;
+        ajax?: AjaxSettings | boolean;
+        completeSubmit: () => void;
+        state?: State;
+        dialog?: BootboxDialogOptions | boolean;
     }
-
     export class Form {
-        private options:FormOptions;
-        private form:JQuery;
-        private data:string;
-
-        constructor(form:JQuery, options?:FormOptions);
-
-        syncOriginal();
-
-        submitForm();
+        private options;
+        private form;
+        private data;
+        private match;
+        constructor(form: JQuery, options?: FormOptions);
+        syncOriginal(): void;
+        getData(): string;
+        submitForm(): void;
+    }
+    export class MicroEvent {
+        private _events;
+        constructor(object?: Object);
+        bind(event: string, fct: any): void;
+        unbind(event: string, fct: any): void;
+        trigger(event: string, ...args: any[]): void;
+    }
+    export interface Handler {
+        handler: string;
+        regex?: RegExp;
+        module: string;
+        exec?: string;
+        ajax?: boolean;
+        pushState?: StateType;
+        permanent?: boolean;
+        widget?: JQuery;
+        menuPath?: string;
+    }
+    export interface BlockUIOptions {
+        message?: string;
+        boxed?: boolean;
+        animate?: boolean;
+        iconOnly?: boolean;
+        textOnly?: boolean;
+        target?: JQuery | string;
+        zIndex?: number;
+        cenrerY?: boolean;
+        overlayColor?: string;
+    }
+    export interface ValidationOptions extends JQueryValidation.ValidationOptions {
+        completeSubmit?: () => void;
     }
 }
 
