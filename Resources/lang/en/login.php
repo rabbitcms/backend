f5214cc0666616c4a63328d1af4b0aa7f687c239
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2016-08-29 20:14:02 +0300

    Add trans messages.

--- Resources/lang/en/login.php
+++ Resources/lang/en/login.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'title'    => 'Sign in to your account',
-    'sign_in'  => 'Sign in',
-    'remember' => 'Remember me',
-    'password' => 'Password',
-    'login'    => 'Login'
+    'wrong credentials' => 'E-mail or password was entered incorrectly.',
+    'title'             => 'Sign in to your account',
+    'sign_in'           => 'Sign in',
+    'remember'          => 'Remember me',
+    'password'          => 'Password',
+    'login'             => 'Login',
 ];
