f5fb3ac1afee563675962e0ea2154eb452d8c669
Dmytro Zhurakovskyi <dmytro.zhurakovsky@gmail.com>
2016-09-26 12:03:32 +0300

    Modal fix

--- Resources/views/layouts/modal.blade.php
+++ Resources/views/layouts/modal.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade ajax-modal" tabindex="-1" aria-hidden="true">
+<div class="modal fade ajax-modal" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             @yield('content')
