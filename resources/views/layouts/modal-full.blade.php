cfeced1de453cc8866278267553428205dbd36bf
CaXaPoK <dmytro.zhurakovsky@gmail.com>
2017-06-27 11:26:28 +0300

    added full with modal layout

---
+++ resources/views/layouts/modal-full.blade.php
@@ -0,0 +1,7 @@
+<div class="modal fade" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-full">
+        <div class="modal-content">
+            @yield('content')
+        </div>
+    </div>
+</div>
