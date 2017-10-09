e9dec261884c77e1f750fb729becde6c0ea97f62
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2017-10-09 14:46:57 +0300

    Fix datepicker localization.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -51,6 +51,7 @@ use Illuminate\Support\Facades\Lang;
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js" integrity="sha256-ixVUAy0s+/DoWFGN9kYLK0M2viz7HxiN/REIo65Qsug=" crossorigin="anonymous"></script>
     <script src="{{asset_module('plugins/select2/select2.min.js', 'backend')}}" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js" integrity="sha256-TueWqYu0G+lYIimeIcMI8x1m14QH/DQVt4s9m/uuhPw=" crossorigin="anonymous"></script>
+
     <script src="{{asset_module('plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js', 'backend')}}" type="text/javascript"></script>
     <script src="{{asset_module('plugins/bootstrap-timepicker/js/bootstrap-timepicker.js', 'backend')}}" type="text/javascript"></script>
     <script src="{{asset_module('plugins/datatables/media/js/jquery.dataTables.min.js', 'backend')}}" type="text/javascript"></script>
@@ -72,9 +73,11 @@ use Illuminate\Support\Facades\Lang;
     @if(app()->getLocale() === 'ru')
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.1/localization/messages_ru.min.js" integrity="sha256-zeB7JCDLJZfVpbmGUgBydwtADoHH7xXQ5csK4ZpcmEI=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/locales/bootstrap-datepicker.ru.min.js" integrity="sha256-iGDUwn2IPSzlnLlVeCe3M4ZIxQxjUoDYdEO6oBZw/Go=" crossorigin="anonymous"></script>
+        <script>jQuery(function($){$.fn.datepicker.defaults.language = 'ru';});</script>
     @else
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.1/localization/messages_uk.min.js" integrity="sha256-4Yu+8eUOvMXkgfz/SVviJCCWghnrZUibPvkUu5qz+wQ=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/locales/bootstrap-datepicker.uk.min.js" integrity="sha256-gJ+sB+muQyOKhTNx/XvtaXXkEr88FXSS3xMgtVR3V+E=" crossorigin="anonymous"></script>
+        <script>jQuery(function($){$.fn.datepicker.defaults.language = 'uk';});</script>
     @endif
 
     <script src="{{asset_module('js/metronic.js', 'backend')}}" type="text/javascript"></script>
