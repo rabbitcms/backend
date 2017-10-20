a93e8de8647be38f17f38b1ba31652508ebe5ca1
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2017-10-20 17:14:37 +0300

    Refactor auth middleware.

--- src/Http/Middleware/Authenticate.php
+++ src/Http/Middleware/Authenticate.php
@@ -1,18 +1,29 @@
-<?php namespace RabbitCMS\Backend\Http\Middleware;
+<?php
+declare(strict_types=1);
+
+namespace RabbitCMS\Backend\Http\Middleware;
 
 use Closure;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Illuminate\Contracts\Auth\Factory as AuthFactory;
 use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\View;
 use RabbitCMS\Backend\Annotation\Permissions as PermissionAnnotation;
 use RabbitCMS\Backend\Contracts\HasAccessEntity;
 use ReflectionClass;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use URL;
 
+/**
+ * Class Authenticate
+ *
+ * @package RabbitCMS\Backend\Http\Middleware
+ */
 class Authenticate
 {
     /**
@@ -35,64 +46,67 @@ class Authenticate
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request $request
+     * @param  Request  $request
      * @param  \Closure $next
+     *
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         if ($this->guard->guest()) {
             if ($request->ajax() || $request->wantsJson()) {
                 return response('Unauthorized.', 401);
-            } else {
-                return redirect()->guest(URL::route('backend.auth'));
             }
+
+            return redirect()->guest(URL::route('backend.auth'));
         }
+
         $user = $this->guard->user();
         if (!($user instanceof HasAccessEntity) || count($user->getPermissions()) === 0) {
-            throw new AccessDeniedHttpException;
-        } else {
-            $routeResolver = $request->getRouteResolver();
-            /* @var Route $route */
-            $route = $routeResolver();
-            $action = $route->getAction();
-            if (is_string($action['uses'])) {
-                list($class, $method) = explode('@', $action['uses']);
-                $reader = new AnnotationReader();
-                $class = new ReflectionClass($class);
-                $annotation = $reader->getClassAnnotation($class, PermissionAnnotation::class);
-                if ($annotation instanceof PermissionAnnotation && !$user->hasAccess($annotation->permissions,
-                        $annotation->all)
-                ) {
-                    if ($request->ajax()) {
-                        return new JsonResponse([], Response::HTTP_FORBIDDEN);
-                    } else {
-                        return $this->forbiddenRequest();
-                    }
-                }
-
-                if ($method = $class->getMethod($method)) {
-                    $annotation = $reader->getMethodAnnotation($method, PermissionAnnotation::class);
-                    if ($annotation instanceof PermissionAnnotation && !$user->hasAccess($annotation->permissions,
-                            $annotation->all)
-                    ) {
-                        if ($request->ajax()) {
-                            return new JsonResponse([], Response::HTTP_FORBIDDEN);
-                        } else {
-                            return $this->forbiddenRequest();
-                        }
-                    }
-                }
-            }
+            throw new AccessDeniedHttpException();
         }
+        $routeResolver = $request->getRouteResolver();
+        /* @var Route $route */
+        $route = $routeResolver();
+        $action = $route->getAction();
+        if (is_string($action['uses'])) {
+            [$class] = $segments = explode('@', $action['uses']);
+            $method = count($segments) === 2 ? $segments[1] : '__invoke';
+            $reader = new AnnotationReader();
+            $class = new ReflectionClass($class);
+            $this->checkAnnotation(
+                $request,
+                $user,
+                $reader->getClassAnnotation($class, PermissionAnnotation::class)
+            );
+            $this->checkAnnotation(
+                $request,
+                $user,
+                $reader->getMethodAnnotation($class->getMethod($method), PermissionAnnotation::class)
+            );
+        }
+
         return $next($request);
     }
 
     /**
-     * @return Response
+     * @param Request                   $request
+     * @param HasAccessEntity           $user
+     * @param PermissionAnnotation|null $annotation
+     *
+     * @return void
+     * @throws HttpResponseException
      */
-    protected function forbiddenRequest()
-    {
-        return \Response::view('backend::deny', [], Response::HTTP_FORBIDDEN);
+    protected function checkAnnotation(
+        Request $request,
+        HasAccessEntity $user,
+        PermissionAnnotation $annotation = null
+    ): void {
+        if ($annotation !== null && !$user->hasAccess($annotation->permissions, $annotation->all)) {
+            if ($request->ajax()) {
+                throw new HttpResponseException(new JsonResponse([], Response::HTTP_FORBIDDEN));
+            }
+            throw new HttpResponseException(new Response(View::make('backend::deny'), Response::HTTP_FORBIDDEN));
+        }
     }
 }
